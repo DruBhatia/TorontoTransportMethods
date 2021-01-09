8f6373e89efc0251b04192e8dd2714e80117d3cb
PolarisTM <dhruov.bhatia@mail.utoronto.ca>
2021-01-09 06:36:15 +0200

    Implemented recording of route durations in csv file by reading sample destination coordinates

--- Directions.py
+++ Directions.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 
 uoft_place_id = "ChIJm_0x87g0K4gR93ZadrabHY0"
 union_station_place_id = "ChIJKQDzOA41K4gRajQDdyzD990"
@@ -13,7 +14,8 @@ base_url = "https://maps.googleapis.com/maps/api/directions/json?"
 # mode = "bicycling"
 
 # request = requests.get(
-#     base_url + "origin=place_id:" + uoft_place_id + "&destination=" + test_latlong + "&mode=" + mode + "&key=" + api_key)
+#     base_url + "origin=place_id:" + uoft_place_id + "&destination=" + test_latlong +
+#     "&mode=" + mode + "&key=" + api_key)
 # seconds = request.json()["routes"][0]["legs"][0]["duration"]["value"]
 # minutes = seconds / 60
 # print(f'This trip will take {minutes} minutes when {mode}.')
@@ -43,7 +45,7 @@ def get_walking(origin: str, destination: str) -> int:
     if json_output["status"] == "ZERO_RESULTS":
         return -1
     else:
-        get_shortest_duration(json_output)
+        return get_shortest_duration(json_output)
 
 
 def get_biking(origin: str, destination: str) -> int:
@@ -55,7 +57,7 @@ def get_biking(origin: str, destination: str) -> int:
     if json_output["status"] == "ZERO_RESULTS":
         return -1
     else:
-        get_shortest_duration(json_output)
+        return get_shortest_duration(json_output)
 
 
 def get_transit(origin: str, destination: str) -> int:
@@ -67,7 +69,7 @@ def get_transit(origin: str, destination: str) -> int:
     if json_output["status"] == "ZERO_RESULTS":
         return -1
     else:
-        get_shortest_duration(json_output)
+        return get_shortest_duration(json_output)
 
 
 def get_driving(origin: str, destination: str) -> int:
@@ -79,7 +81,7 @@ def get_driving(origin: str, destination: str) -> int:
     if json_output["status"] == "ZERO_RESULTS":
         return -1
     else:
-        get_shortest_duration(json_output)
+        return get_shortest_duration(json_output)
 
 
 def all_transport_modes(origin: str, destination: str) -> list:
@@ -93,3 +95,32 @@ def all_transport_modes(origin: str, destination: str) -> list:
         ]
     return durations
 
+
+def record_durations(origin: str, input_file: str, output_file: str) -> None:
+    """Record the durations of the shortest trips using each mode of transport from origin to the destinations
+    stored in the rows of the input_file by writing the durations corresponding to each destination into the
+    output file. BOTH input_file AND output_file MUST BE EXISTING .CSV FILES."""
+    with open(input_file) as csv_in:
+        csv_reader = csv.reader(csv_in)
+        line_count = 0
+        with open(output_file, 'w', newline='') as csv_out:
+            csv_writer = csv.writer(csv_out)
+            csv_writer.writerow(['TRACTID', 'LONGITUDE', 'LATITUDE', 'WALK', 'BIKE', 'TRANSIT', 'DRIVE', 'SHORTEST'])
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    tract_id = row[0]
+                    long = row[1]
+                    lat = row[2]
+                    destination = lat + "," + long
+                    durations = all_transport_modes(origin, destination)
+                    walk, bike, transit, drive = durations
+                    for i in range(0, len(durations)):
+                        if min(durations) == durations[i]:
+                            shortest = i
+                    full_row = tract_id, long, lat, walk, bike, transit, drive, shortest
+                    csv_writer.writerow(full_row)
+
+
+record_durations(uoft_place_id, "sample_centroids.csv", "sample_durations.csv")
